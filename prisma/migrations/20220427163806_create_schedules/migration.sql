91b5b2b5b241ae7ef0c3d64bb93aabb6513d81be
jeferson-bsi <jefersoncou18@gmail.com>
2022-04-27 18:51:13 -0300

    deploy

---
+++ prisma/migrations/20220427163806_create_schedules/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "schedules" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "date" TEXT NOT NULL,
+    "time" TEXT NOT NULL,
+    "type" TEXT NOT NULL
+);
